612bad7c8f7ba41cb6fc4a95f3484b5398f6e98b
All1in <bm3023068@gmail.com>
2023-04-14 18:41:35 +0300

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
 import './styles/App.css'
 import { Input, Button } from 'antd';
-import { useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import UserCard from "./components/UserCard/UserCard";
 import { useState } from "react";
+import { addUser } from "./redux/reducers/Users/Users";
 const { Search } = Input;
 
 const App = () => {
+  const dispatch = useDispatch()
   const userList = useSelector((state) => state.users.value)
   const [name, setName] = useState('')
   const [username, setUsername] = useState('')
   const [newUsername, setNewUsername] = useState('')
   const onSearch = (event) =>  setUsername(event.target.value)
 
+  const handleButton = () => {
+      dispatch(
+          addUser({
+              id: userList[userList.length - 1].id + 1,
+              name,
+              username,
+          })
+      )
+      console.log('User created')
+  }
+
   return (
     <div className="App">
         <div className='addUser'>
             <Search
                 placeholder='Name...'
-                onSearch={onSearch}
+                onChange={onSearch}
                 enterButton
             />
             <Search
                 placeholder='Username...'
-                onSearch={onSearch}
+                onChange={onSearch}
                 enterButton
             />
-            <Button type="primary" danger> Add User </Button>
+            <Button onClick={handleButton} type="primary" danger> Add User </Button>
         </div>
         <div className='displayUsers'>
             {userList.map((user) => {
